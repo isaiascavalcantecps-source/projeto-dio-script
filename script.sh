872167febb37c725220a2bc26b84a0ea9e9c6dc5
Isaias Cavalcante <isaiascavalcantecps@gmail.com>
2025-09-03 20:02:10 -0300

    Adicionado as permissões dos diretórios aos seus respectivos grupos

--- script.sh
+++ script.sh
@@ -35,3 +35,31 @@ echo "    "
 echo "========================================"
 echo " Grupos de usuarios criados com sucesso"
 echo "========================================"
+echo "    "
+echo "    "
+echo "    "
+echo "    "
+echo "    "
+echo "========================================"
+echo " Setando permissão das pastas aos grupos"
+echo "========================================"
+chown root:GRP_ADM /adm/
+chmod 770 /adm/
+echo "Pasta ADM vinculada apenas ao grupo ADM"
+echo "    "
+chown root:GRP_VEN /ven/
+chmod 770 /ven/
+echo "Pasta VEN vinculada apenas ao grupo VEN"
+echo "    "
+chown root:GRP_SEC /sec/
+chmod 770 /sec/
+echo "Pasta SEC vinculada apenas ao grupo SEC"
+echo "    "
+chown root:root /publico
+chmod 777 /publico
+echo "Pasta VEN vinculada apenas ao grupo VEN"
+echo "    "
+echo "    "
+echo "    "
+echo "    "
+echo "    "
