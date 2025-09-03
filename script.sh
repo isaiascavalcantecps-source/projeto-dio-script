c658feb5fe5105ff5ed1c42a71c4b7e649a52e51
Isaias Cavalcante <isaiascavalcantecps@gmail.com>
2025-09-03 19:22:25 -0300

    Arquivo script.sh gerado, script para criar diretórios finalizado.

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "========================================"
+echo "          Criando diretorios"
+echo "========================================"
+echo "     "
+mkdir /publico
+echo " Diretorio público criado."
+mkdir /adm
+echo " Diretório adm criado."
+mkdir /ven
+echo " Diretório ven criado."
+mkdir /sec
+echo " Diretório sec criado."
+echo "   "
